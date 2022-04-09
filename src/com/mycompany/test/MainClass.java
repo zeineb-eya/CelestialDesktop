5afafb79ffce55a421896dee91f41ed58b5cb73a
Zeineb Eya Rahmani <zeineb.rahmani@esprit.tn>
2022-04-09 15:32:53 +0100

    removed unused files

--- src/com/mycompany/test/MainClass.java
+++ src/com/mycompany/test/MainClass.java
@@ -9,58 +9,21 @@ import com.mycompany.entities.Reservation;
 import com.mycompany.services.BilletService;
 import com.mycompany.services.ReservationService;
 import com.mycompany.utils.MyConnection;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
+
 /**
  *
  * @author HP
  */
-public class MainClass /*extends Application */{
-//     private double xOffset = 0;
-//    private double yOffset = 0;
-//
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        Font.loadFont(MainClass.class.getResource("/com/mycompany/gui/fonts/Roboto-Bold.ttf").toExternalForm(), 10);
-//        stage.getIcons().add(new Image("/com/mycompany/gui/img/icon.png"));
-//        stage.setTitle("Celestial");
-//
-////        Parent root = FXMLLoader.load(getClass().getResource("/com/mycompany/gui/profile.fxml"));
-//        //Parent root = FXMLLoader.load(getClass().getResource("/com/mycompany/gui/login.fxml"));
-//        Parent root = FXMLLoader.load(getClass().getResource("/com/mycompany/gui/addbillet.fxml"));
-//
-//        Scene scene = new Scene(root, 1280, 720);
-////        stage.initStyle(StageStyle.UNDECORATED);
-//
-//        root.setOnMousePressed((MouseEvent event) -> {
-//            xOffset = event.getSceneX();
-//            yOffset = event.getSceneY();
-//        });
-//        root.setOnMouseDragged((MouseEvent event) -> {
-//            stage.setX(event.getScreenX() - xOffset);
-//            stage.setY(event.getScreenY() - yOffset);
-//        });
-//
-//        stage.setScene(scene);
-////        stage.setResizable(false);
-//        stage.show();
-//    }
+public class MainClass{
 
     public static void main(String[] args) {
-         //Application.launch(args);
         //******************************************Test Connection******************************************//
         // MyConnection mc = new MyConnection();
 //        MyConnection mc = MyConnection.getInstance();
 //        MyConnection mc2 = MyConnection.getInstance();
 //        System.out.println(mc.hashCode()+ " - " +mc2.hashCode());
 
-        BilletService bs = new BilletService();
+      BilletService bs = new BilletService();
       ReservationService rs = new ReservationService();
         //******************************************ajout statique******************************************//
         //**Billet**//
