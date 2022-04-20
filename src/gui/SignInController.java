2f873daee37c01a7f81c78e70ae86a15b12e55f2
unknown <yacoubi.fatima@esprit.tn>
2022-04-20 23:03:52 +0100

    login front+back

--- src/gui/SignInController.java
+++ src/gui/SignInController.java
@@ -5,24 +5,21 @@
  */
 package gui;
 
-import entities.User;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import services.UserService;
 import util.MyDB;
 
 /**
@@ -31,18 +28,30 @@ import util.MyDB;
  * @author Cipher
  */
 public class SignInController implements Initializable {
-@FXML
-    private TextField username;
- 
+
     /**
      * Initializes the controller class.
      */
+     Connection con = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    @FXML
+    private TextField username;
+     @FXML
+    private TextField tfmailU;
+      @FXML
+    private TextField tfpasswordU;
+       @FXML
+    private Button btnSignin;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
      @FXML
     private void Login(ActionEvent event) {
+         if (event.getSource() == btnSignin) {
+            //login here
+            if (logIn().equals("admin")) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Choix.fxml"));
             Parent root = loader.load();
@@ -51,8 +60,56 @@ public class SignInController implements Initializable {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+    } else if (logIn().equals("Success")) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("InformationUser.fxml"));
+            Parent root = loader.load();
+            InformationUserController controller = loader.getController();
+            username.getScene().setRoot(root);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }}
+            
+            
+            
+            else {Alert a = new Alert(Alert.AlertType.ERROR);
+        a.setTitle("Verifiez vos données.");
+                 String content = String.format("Il n'ya pas un compte contenant ce mot de passe et adresse");
+        a.setContentText(content);
+                  a.show();}}}
+      public SignInController() {
+        con = MyDB.getInstance().getConnection();
+    }
+        private String logIn() {
+        String status = "Success";
+        String email = tfmailU.getText();
+        String password = tfpasswordU.getText();
+        if(email.isEmpty() || password.isEmpty()) {
+            status = "Error";
+        } else if(email.equalsIgnoreCase("Admin") && password.equalsIgnoreCase("0000")) 
+        { status ="admin";
+        }
+        
+        else {
+            //query
+            String sql = "SELECT * FROM user Where mail_utilisateur = ? and password = ?";
+            try {
+                preparedStatement = con.prepareStatement(sql);
+                preparedStatement.setString(1, email);
+                preparedStatement.setString(2, password);
+                resultSet = preparedStatement.executeQuery();
+                if (!resultSet.next()) {
+                    status = "Error";
+                } else {
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+                status = "Exception";
+            }
+        }
+        
+        return status;
     }
     
-}
-
     
+}
