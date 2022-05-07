f3b4bb362b66100013af77c1997b1ea37bfb6434
unknown <yacoubi.fatima@esprit.tn>
2022-05-07 14:35:27 +0100

    Link resolu
    Co-Authored-By: zainebbachouch <48316919+zainebbachouch@users.noreply.github.com>

--- src/gui/ChoixController.java
+++ src/gui/ChoixController.java
@@ -15,8 +15,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -79,18 +85,42 @@ public class ChoixController implements Initializable {
             System.out.println(ex.getMessage());
         }
     }
-       @FXML
+        
+    
+     @FXML
+    private void onClickCommentaire_btn(ActionEvent event) {
+               this.getForm ("../gui//Commentaire/Commentaire_FXML.fxml");
+    }
+    @FXML
+    private void onClickPost_btn(ActionEvent event) {
+               this.getForm ("../gui//Post/Post_FXML.fxml");
+    }
+    @FXML
+    private void onClickCatPost_btn(ActionEvent event) {
+               this.getForm ("../gui//CategoriePost/CategoriePost_FXML.fxml");
+    }
+         @FXML
+private void getForm (String src) {
+                  try {                            System.out.println("getForm1");
 
-     private void catPost(ActionEvent event) {
-         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Commentaire_FXML.fxml"));
-            
-            Parent root = loader.load();
-            Commentaire_FXMLController controller = loader.getController();
-            tfmailU.getScene().setRoot(root); 
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+                           Parent root = FXMLLoader.load(getClass().getResource(src)); 
+
+                           Stage stage = new Stage();
+                           
+                           Scene scene = new Scene(root);
+                           stage.setScene(scene);  
+                           System.out.println("getForm");
+                           // Specifies the modality for new window.
+                           stage.initModality(Modality.WINDOW_MODAL);  
+                           stage.setTitle("Edit");
+                           Image mylmage = new Image( "img/gui_edit_icon.png" );
+                           stage.getIcons().add( mylmage );// tasswira 3ala jnab fo9 ki na3mil min fo9 twali auto min lota*/ 
+                           Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+                           stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
+                           stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2); 
+                           stage.show(); 
+                      } 
+                  catch ( Exception ex) {   System.out.println("getForm : "+ex.getMessage());  }   
     }
      
 }
