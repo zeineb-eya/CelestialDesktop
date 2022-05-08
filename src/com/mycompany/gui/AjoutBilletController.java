e30af25be2feff6c791985244d6876452173bd8d
Zeineb Eya Rahmani <zeineb.rahmani@esprit.tn>
2022-05-08 19:40:00 +0100

    join billet

--- src/com/mycompany/gui/AjoutBilletController.java
+++ src/com/mycompany/gui/AjoutBilletController.java
@@ -7,12 +7,21 @@ package com.mycompany.gui;
 
 import com.mycompany.entities.Billet;
 import com.mycompany.services.BilletService;
+import com.mycompany.utils.MyConnection;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,8 +30,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -44,12 +55,13 @@ public class AjoutBilletController implements Initializable {
     private TextField portail;
     @FXML
     private DatePicker embarquement;
-    @FXML
     private TextField localisation;
     @FXML
     private Button AjouterBillet;
     @FXML
     private Button ListerBillets;
+    @FXML
+    private ComboBox<String> localisationcombo;
 
     /**
      * Initializes the controller class.
@@ -83,8 +95,26 @@ public class AjoutBilletController implements Initializable {
         b.setTerminal(Integer.parseInt(terminal.getText()));
         b.setPortail(Integer.parseInt(portail.getText()));
         b.setEmbarquement(Date.valueOf(embarquement.getValue()));
-        b.setLocalisation(Integer.parseInt(localisation.getText()));
-        
+       // b.setLocalisation(Integer.parseInt(localisation.getText()));
+         b.setLocalisation(Integer.parseInt(localisationcombo.getValue().toString().split("_")[0]));
+         String localisationid =  localisationcombo.getValue().toString();
+
+         String sql2="select billet.localisation_id from billet join localisation ON billet.localisation_id = localisation.id where localisation.id";
+                int localisation_id=0;
+                  try {
+                Statement ste2;
+                Connection cnx2;
+                cnx2 = MyConnection.getInstance().getCnx();
+                ste2 = cnx2.prepareStatement(sql2);
+                ResultSet rs2 = ste2.executeQuery(sql2);
+                if(rs2.next())
+                {
+                  localisation_id=rs2.getInt("localisation_id");
+                    
+                }
+            } catch (SQLException ex) {
+               System.out.println(ex.getMessage());
+            }
         BilletService bs = new BilletService();
         bs.ajouterBiller2(b);
         Alert a = new Alert(Alert.AlertType.CONFIRMATION);
@@ -105,6 +135,27 @@ public class AjoutBilletController implements Initializable {
             stage.show();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
+        }
+    }
+
+    @FXML
+    private void remplirCB(MouseEvent event) {
+        try {
+            String sql="select id from localisation ";
+            List<String> nm =new ArrayList<String>();
+            PreparedStatement ste;
+            Statement st;
+            Connection cnx;
+            cnx = MyConnection.getInstance().getCnx();
+            st = cnx.prepareStatement(sql);
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                nm.add(rs.getString("id"));
+                  localisationcombo.setItems(FXCollections.observableArrayList(nm));
+            }
+        } catch (SQLException ex) {
+             System.out.println(ex.getMessage());
         }
     }
     
